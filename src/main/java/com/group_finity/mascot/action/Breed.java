2fc8602309d65c1fa8f320124305083df967cdc3
Кремлев Святослав Юрьевич <kremlevsy@it.mos.ru>
2024-12-25 12:01:33 +0300

    fix path

--- src/main/java/com/group_finity/mascot/action/Breed.java
+++ src/main/java/com/group_finity/mascot/action/Breed.java
@@ -21,7 +21,7 @@ import com.group_finity.mascot.script.VariableMap;
 public class Breed extends Animate
 {
     private static final Logger log = Logger.getLogger( Breed.class.getName( ) );
-    
+
     // thanks to LavenderSnek for the idea for this delegate, cleans up the breeding code nicely
     static class Delegate
     {
@@ -45,14 +45,14 @@ public class Breed extends Animate
 
         public static final String PARAMETER_BORNCOUNT = "BornCount";
         private static final int DEFAULT_BORNCOUNT = 1;
-        
+
         private final ActionBase action;
-        
+
         Delegate( ActionBase action )
         {
             this.action = action;
         }
-        
+
         boolean isEnabled( ) throws VariableException
         {
             return ( getBornTransient( ) ?
@@ -64,17 +64,17 @@ public class Breed extends Animate
         {
             return action.getTime( ) % getBornInterval( ) == 0;
         }
-        
+
         boolean isPenultimateFrame( ) throws VariableException
         {
             return action.getTime( ) == action.getAnimation( ).getDuration( ) - 1;
         }
-        
+
         void breed( ) throws VariableException
         {
             double scaling = Double.parseDouble( Main.getInstance( ).getProperties( ).getProperty( "Scaling", "1.0" ) );
             String childType = Main.getInstance( ).getConfiguration( getBornMascot( ) ) != null ? getBornMascot( ) : action.getMascot( ).getImageSet( );
-        
+
             for( int index = 0; index < getBornCount( ); index++ )
             {
                 final Mascot mascot = new Mascot( childType );
@@ -88,7 +88,7 @@ public class Breed extends Animate
                 }
                 else
                 {
-                    mascot.setAnchor( new Point( action.getMascot( ).getAnchor( ).x + ( (int)Math.round( getBornX( ) * scaling ) ), 
+                    mascot.setAnchor( new Point( action.getMascot( ).getAnchor( ).x + ( (int)Math.round( getBornX( ) * scaling ) ),
                                                  action.getMascot( ).getAnchor( ).y + ( (int)Math.round( getBornY( ) * scaling ) ) ) );
                 }
                 mascot.setLookRight( action.getMascot( ).isLookRight( ) );
@@ -142,7 +142,7 @@ public class Breed extends Animate
 
         private String getBornMascot( ) throws VariableException
         {
-            return action.eval( action.getSchema( ).getString( PARAMETER_BORNMASCOT ), String.class, DEFAULT_BORNMASCOT );
+            return action.eval( action.getSchema( ).getString( PARAMETER_BORNMASCOT ), String.class, DEFAULT_BORNMASCOT ).replace("\\","/");
         }
 
         private boolean getBornTransient( ) throws VariableException
@@ -160,9 +160,9 @@ public class Breed extends Animate
             return action.eval( action.getSchema( ).getString( PARAMETER_BORNCOUNT ), Number.class, DEFAULT_BORNCOUNT ).intValue( );
         }
     }
-    
+
     private final Delegate delegate = new Delegate( this );
-    
+
     public Breed( java.util.ResourceBundle schema, final List<Animation> animations, final VariableMap context )
     {
         super( schema, animations, context );
@@ -172,7 +172,6 @@ public class Breed extends Animate
     public void init( final Mascot mascot ) throws VariableException
     {
         super.init( mascot );
-        
         delegate.validateBornCount( );
     }
 
